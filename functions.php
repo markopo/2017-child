6b35ab7597472587ee43e3cb3ec27f8030358601
Marko Poikkimäki <markopoikkimaki@Markos-MBP.lan>
2017-10-30 21:14:29 +0100

    2017 child theme + books custom post type

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+function create_post_type() {
+    register_post_type( 'books',
+        array(
+            'labels' => array(
+                'name' => 'Books',
+                'singular_name' => 'Books'
+            ),
+            'public' => true,
+            'has_archive' => true,
+        )
+    );
+}
+add_action( 'init', 'create_post_type' );
---
+++ template-parts/post/book.php
@@ -0,0 +1,84 @@
+<?php
+   $postId = get_the_ID();
+   $isbn10 = get_field('isbn_10', $postId);
+   $isbn13 = get_field('isbn-13', $postId);
+   $amazon_link = get_field('amazon_link', $postId);
+   $author = get_field('author', $postId);
+
+   function book_title() {
+        if ( is_single() ) {
+            the_title( '<h1 class="entry-title">', '</h1>' );
+        } elseif ( is_front_page() && is_home() ) {
+            the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+        } else {
+            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+        }
+   }
+
+?>
+
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php
+    if ( is_sticky() && is_home() ) :
+        echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
+    endif;
+    ?>
+    <header  class="entry-header">
+        <?php if($amazon_link != false): ?>
+        <a href="<?php echo $amazon_link; ?>">
+        <?php endif; ?>
+
+        <?php book_title(); ?>
+
+        <?php if($amazon_link != false): ?>
+        </a>
+        <?php endif; ?>
+
+        <?php if($author != false): ?>
+        <p>By <?php echo $author; ?></p>
+        <?php endif; ?>
+    </header><!-- .entry-header -->
+
+    <?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+        <div class="post-thumbnail">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+            </a>
+        </div><!-- .post-thumbnail -->
+    <?php endif; ?>
+
+    <div class="entry-content">
+        <?php
+        /* translators: %s: Name of current post */
+        the_content( sprintf(
+            __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+            get_the_title()
+        ) );
+
+        wp_link_pages( array(
+            'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+            'after'       => '</div>',
+            'link_before' => '<span class="page-number">',
+            'link_after'  => '</span>',
+        ) );
+        ?>
+    </div><!-- .entry-content -->
+    <div>
+        <?php if($isbn10 != false): ?>
+            <p>ISBN-10: <?php echo $isbn10; ?></p>
+        <?php endif; ?>
+
+        <?php if($isbn13 != false): ?>
+            <p>ISBN-13: <?php echo $isbn13; ?></p>
+        <?php endif; ?>
+    </div>
+
+    <?php
+    if ( is_single() ) {
+        twentyseventeen_entry_footer();
+    }
+    ?>
+
+</article><!-- #post-## -->
